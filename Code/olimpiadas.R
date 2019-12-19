01ef1a6a13e3286e4f5f08f79bebcd82710bc3af
mbs76 <mbetancor@uoc.edu>
2019-12-19 12:15:18 +0000

    Update olimpiadas.R

--- Code/olimpiadas.R
+++ Code/olimpiadas.R
@@ -1,14 +1,14 @@
 # 
 
 #Importamos los diferentes dataset del proyecto
-#Es importante modificar la ruta de ubicación de cada uno de los archivos
 #Los ficheros csv a importar se han adquirido a través de los siguientes enlaces:
 #  https://www.kaggle.com/heesoo37/120-years-of-olympic-history-athletes-and-results
 #  https://www.kaggle.com/statchaitya/country-to-continent
 #  https://www.downloadexcelfiles.com/wo_en/download-excel-file-list-olympic-host-cities
+#Para la carga se va a utilizar enlaces suministrados por dropbox de los archivos
 
+atletas <- read.csv("https://www.dropbox.com/s/8ogvaf0ipu4raby/athlete_events.csv?dl=1")
+noc <- read.csv("https://www.dropbox.com/s/hjngggjiwmsdyw8/noc_regions.csv?dl=1")
+paises <- read.csv("https://www.dropbox.com/s/qol9t3g4z359img/countryContinent.csv?dl=1")
+ciudades <- read.csv("https://www.dropbox.com/s/6fhwd3ydoeop30j/list-host-cities-olympic-943j.csv?dl=1")
 
-atletas <- read.csv("athlete_events.csv")
-noc <- read.csv("noc_regions.csv")
-paises <- read.csv("countryContinent.csv")
-ciudades <- read.csv("list-host-cities-olympic-943j.csv")
