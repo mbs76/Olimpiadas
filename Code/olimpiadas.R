2d2d1a833cb6d2d773c92465a4e45768e6ed6c2f
María Navalón <mdnavalon@gmail.com>
2019-12-19 17:55:27 +0100

    Update olimpiadas.R

--- Code/olimpiadas.R
+++ Code/olimpiadas.R
@@ -1,14 +1,15 @@
 # 
 
 #Importamos los diferentes dataset del proyecto
-#Los ficheros csv a importar se han adquirido a través de los siguientes enlaces:
+#Los ficheros csv a importar se han adquirido a travÃ©s de los siguientes enlaces:
 #  https://www.kaggle.com/heesoo37/120-years-of-olympic-history-athletes-and-results
 #  https://www.kaggle.com/statchaitya/country-to-continent
 #  https://www.downloadexcelfiles.com/wo_en/download-excel-file-list-olympic-host-cities
 #Para la carga se va a utilizar enlaces suministrados por dropbox de los archivos
 
-atletas <- read.csv("https://www.dropbox.com/s/8ogvaf0ipu4raby/athlete_events.csv?dl=1")
-noc <- read.csv("https://www.dropbox.com/s/hjngggjiwmsdyw8/noc_regions.csv?dl=1")
-paises <- read.csv("https://www.dropbox.com/s/qol9t3g4z359img/countryContinent.csv?dl=1")
-ciudades <- read.csv("https://www.dropbox.com/s/6fhwd3ydoeop30j/list-host-cities-olympic-943j.csv?dl=1")
+atletas <- read.csv("https://www.dropbox.com/s/8ogvaf0ipu4raby/athlete_events.csv?dl=1", encoding="utf-8")
+noc <- read.csv("https://www.dropbox.com/s/hjngggjiwmsdyw8/noc_regions.csv?dl=1", encoding="utf-8")
+paises <- read.csv("https://www.dropbox.com/s/qol9t3g4z359img/countryContinent.csv?dl=1", encoding="utf-8")
+ciudades <- read.csv("https://www.dropbox.com/s/6fhwd3ydoeop30j/list-host-cities-olympic-943j.csv?dl=1", encoding="utf-8")
+
 
